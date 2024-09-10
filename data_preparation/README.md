bb8ef37827df033242e16dc660fc86605001f753
Junior flores <fmjg21@gmail.com>
2024-09-10 01:27:49 -0500

    refactoring the structure

---
+++ data_preparation/README.md
@@ -0,0 +1,6 @@
+# data process
+
+Section to prepare data for the web application
+- upload tiff files
+- create csv format
+- validate csv format
--- web_app/src/pages/index.jsx
+++ web_app/src/pages/index.jsx
@@ -4,6 +4,7 @@ import ReactMarkdown from 'react-markdown';
 
 import StaticMap, { NavigationControl, ScaleControl } from "react-map-gl";
 const MAPBOX_ACCESS_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+const MAPBOX_STYLE = process.env.NEXT_PUBLIC_MAPBOX_STYLE;
 
 const initialViewState = {
   latitude: 14.0583,
@@ -14,11 +15,7 @@ const initialViewState = {
 export default function Home({ content }) {
   const mapRef = useRef(null);
   const [viewState, setViewState] = useState({ ...initialViewState });
-  const [sourcesData, setSourcesData] = useState(null);
-  const [sourcesDataFlag, setSourcesDataFlag] = useState(null);
-  const [hoverInfo, setHoverInfo] = useState(null);
-  const [osmInfo, setOsmInfo] = useState(null);
-  console.log(content)
+  
   return (
     <main className="relative h-screen bg-white w-75 dark:bg-slate-800">
            <h1>{content.homepage.title}</h1>
@@ -30,7 +27,7 @@ export default function Home({ content }) {
           // onLoad={handleLoad}
           minZoom={6}
           maxZoom={15}
-          mapStyle=""
+          mapStyle={MAPBOX_STYLE}
           mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
         >
           <ScaleControl position="top-left" />
