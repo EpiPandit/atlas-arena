73dbc634db16aee53bbca7b7dc5dde80d5dcb238
Junior flores <fmjg21@gmail.com>
2024-09-10 00:50:01 -0500

    update libs

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,28 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState } from "react";
+import { loadContent } from '@/libs/loadYamlContent';
+import ReactMarkdown from 'react-markdown';
 
-import StaticMap, { NavigationControl, ScaleControl } from 'react-map-gl';
-const API_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+import StaticMap, { NavigationControl, ScaleControl } from "react-map-gl";
+const MAPBOX_ACCESS_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 
 const initialViewState = {
   latitude: 14.0583,
   longitude: 108.2772,
-  zoom: 7
+  zoom: 7,
 };
 
-export default function Home() {
+export default function Home({ content }) {
   const mapRef = useRef(null);
   const [viewState, setViewState] = useState({ ...initialViewState });
   const [sourcesData, setSourcesData] = useState(null);
   const [sourcesDataFlag, setSourcesDataFlag] = useState(null);
   const [hoverInfo, setHoverInfo] = useState(null);
   const [osmInfo, setOsmInfo] = useState(null);
-
+  console.log(content)
   return (
-   <main className="relative w-full h-screen bg-white dark:bg-slate-800">
+    <main className="relative h-screen bg-white w-75 dark:bg-slate-800">
+           <h1>{content.homepage.title}</h1>
+           <ReactMarkdown>{content.homepage.body}</ReactMarkdown>
       <div className="w-screen h-screen">
         <StaticMap
           ref={mapRef}
@@ -27,13 +31,21 @@ export default function Home() {
           minZoom={6}
           maxZoom={15}
           mapStyle=""
-          mapboxAccessToken={API_TOKEN}
+          mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
         >
-          
           <ScaleControl position="top-left" />
           <NavigationControl position="top-left" />
         </StaticMap>
       </div>
     </main>
   );
+}
+
+export async function getStaticProps() {
+  const content = loadContent("content.yaml");
+  return {
+    props: {
+      content,
+    },
+  };
 }
