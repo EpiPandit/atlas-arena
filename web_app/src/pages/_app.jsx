032b704372681b611afa95913a154f1cea39163c
Junior flores <fmjg21@gmail.com>
2024-09-15 00:08:04 -0500

    add chakra

--- web_app/src/pages/_app.jsx
+++ web_app/src/pages/_app.jsx
@@ -1,6 +1,12 @@
 import '@/assets/css/globals.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '@/config/theme';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
