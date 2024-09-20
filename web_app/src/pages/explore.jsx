6f44b6a5a5b9530d8886a4cb818340fcc8a5109f
Junior flores <fmjg21@gmail.com>
2024-09-19 20:39:07 -0500

    add admin boundaries

--- web_app/src/pages/explore.jsx
+++ web_app/src/pages/explore.jsx
@@ -10,9 +10,12 @@ import { useAppContext } from '@/store/context';
 import { dynamicFilter } from '@/utils/utils';
 import Sidebar from '@/components/Sidebar';
 import { defaultMapColor } from '@/utils/mapStyle';
+import axios from 'axios';
+import pako from 'pako';
 
 const MAPBOX_ACCESS_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 const MAPBOX_STYLE = process.env.NEXT_PUBLIC_MAPBOX_STYLE;
+const BASENAME = (process.env.PUBLIC_URL || '').replace('//', '/');
 
 const initialViewState = {
   latitude: 8.44830069039078,
@@ -26,6 +29,7 @@ const Explore = () => {
   const mapRef = useRef(null);
   const [viewState, setViewState] = useState({ ...initialViewState });
   const [filterTilesId, setFilterTilesId] = useState([]);
+  const [boundariesAdm, setBoundariesAdm] = useState(null);
 
   const handleFilterTilesId = (data_filter) => {
     console.log({ ...data_filter });
@@ -34,7 +38,27 @@ const Explore = () => {
     console.log(raw_data_filter.length, raw_data_filter);
     setFilterTilesId(raw_data_filter);
   };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `${BASENAME}/assets/filter_sa.geojson.gz`,
+          {
+            responseType: 'arraybuffer',
+          }
+        );
+        const decompressed = pako.inflate(response.data, { to: 'string' });
+        const jsonDataMapp = JSON.parse(decompressed);
+        if (jsonDataMapp && jsonDataMapp.features) {
+          setBoundariesAdm(jsonDataMapp);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
 
+    fetchData();
+  }, []);
   // const [initialState, setInitialState] = useState({ ...data });
 
   // const handleLoad = () => {
@@ -64,9 +88,11 @@ const Explore = () => {
             'raster-opacity': 0.8,
             'raster-color': [...defaultMapColor],
           }}
+          beforeId='adm_boundaries_layer'
         />
       </Source>
     ));
+  const hasTilesId = filterTilesId.length !== 0;
   return (
     <Flex flexDirection='row'>
       <Sidebar
@@ -84,6 +110,19 @@ const Explore = () => {
             mapStyle={MAPBOX_STYLE}
             mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
           >
+            {boundariesAdm && (
+              <Source id='adm_boundaries' type='geojson' data={boundariesAdm}>
+                <Layer
+                  id='adm_boundaries_layer'
+                  type='line'
+                  source='adm_boundaries'
+                  paint={{
+                    'line-width': hasTilesId ? 0.5 : 0,
+                    'line-color': 'rgba(255, 255, 255, 0.8)',
+                  }}
+                />
+              </Source>
+            )}
             {buildRender}
             <ScaleControl position='top-right' />
             <NavigationControl position='top-right' />
