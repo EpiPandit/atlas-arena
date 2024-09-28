a93e69cfa9973b42455d79cb1e5e11b29b08e0b5
Junior flores <fmjg21@gmail.com>
2024-09-28 15:11:12 -0500

    initial about

--- web_app/src/pages/about.jsx
+++ web_app/src/pages/about.jsx
@@ -1,30 +1,33 @@
 import React, { useRef, useEffect, useState } from 'react';
 // import { loadContent } from '@/libs/yaml';
-import { Container } from '@chakra-ui/react';
+import { Container, Box, Heading, Text } from '@chakra-ui/react';
 import ReactMarkdown from 'react-markdown';
 import ChakraUIRenderer from 'chakra-ui-markdown-renderer';
 
 export default function Home({ content }) {
   const { title, description, sections } = content;
   return (
-    <Container
-      maxW={[
-        'container.sm',
-        'container.md',
-        'container.lg',
-        'container.xl',
-        'container.2xl',
-      ]}
-      p={[2, 4]}
-    >
-      <h1>{title}</h1>
-      <h2>{title}</h2>
-      <h3>{title}</h3>
-      <ReactMarkdown
-        components={ChakraUIRenderer()}
-        children={description}
-        skipHtml
-      />
+    <Container maxW='container.lg' p={2}>
+      <Box my={4}>
+        <Text
+          fontSize='xl'
+          my={2}
+          textTransform='uppercase'
+          fontStyle='italic'
+          color='blue.800'
+        >
+          About
+        </Text>
+        <Text
+          fontSize='lg'
+          my={2}
+          textTransform='uppercase'
+          fontStyle='italic'
+          color='blue.800'
+        >
+          Team
+        </Text>
+      </Box>
     </Container>
   );
 }
