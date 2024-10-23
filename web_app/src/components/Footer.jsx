f95a67fd390eb7042fe5950b07ac063c4d43fd02
Junior flores <fmjg21@gmail.com>
2024-10-23 12:55:39 -0500

    add geocompas logo

--- web_app/src/components/Footer.jsx
+++ web_app/src/components/Footer.jsx
@@ -2,12 +2,14 @@ import { Box, Flex, Text, Image } from '@chakra-ui/react';
 import wellcomeLogo from '@/assets/images/wellcome.png';
 import ucdavisLogo from '@/assets/images/ucdavis.jpg';
 import devseedLogo from '@/assets/images/devseed.png';
+import geocompasLogo from '@/assets/images/geocompas.webp';
 
 const Footer = () => {
   return (
     <Box
       py={3}
-      my={6}
+      mt={4}
+      mb={8}
       textAlign='center'
       minH='150px'
       w='100%'
@@ -26,7 +28,8 @@ const Footer = () => {
       <Flex
         justifyContent='center'
         alignItems='center'
-        gap={{ base: 4, md: 10 }}
+        gap={{ base: 3, md: 6, sm: 2 }}
+        mb={2}
         wrap='wrap'
         px={4}
         w='100%'
@@ -47,6 +50,12 @@ const Footer = () => {
           src={devseedLogo.src}
           alt='Development Seed logo'
           w={{ base: '130px', md: '186px' }}
+          objectFit='contain'
+        />
+        <Image
+          src={geocompasLogo.src}
+          alt='Geocompas'
+          w={{ base: '150px', md: '186px' }}
           objectFit='contain'
         />
       </Flex>
